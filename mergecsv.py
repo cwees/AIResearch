6e9948ac2f8674b56c3b174086313dd221e7715a
chrisdesktopreplacement <chrishe44@gmail.com>
2024-02-06 23:51:25 -0600

    add file merger and duplicate dropper

--- mergecsv.py
+++ mergecsv.py
@@ -1,18 +1,37 @@
 import pandas as pd
 import os
 
-def main():
-    file_path = "input/copilot/"
+
+def all():
+    stuff = [
+        "all",
+        "bard",
+        "elevenlabs",
+        "copilot",
+        "jasper",
+        "midjourney",
+        "openai",
+        "stablediffusion",
+    ]
+    for file in stuff:
+        merge(file)
+
+
+def merge(dir):
+    file_path = dir + "/"
     file_list = os.listdir(file_path)
     print(file_list)
     df_append = pd.DataFrame()
     for file in file_list:
-        df_temp = pd.read_csv(file_path+file)
+        df_temp = pd.read_csv(file_path + file, lineterminator="\n")
         df_append = df_append._append(df_temp, ignore_index=True)
 
-    print(df_append.size)
-    df_append.drop_duplicates(inplace=True)
-    print(df_append.size)
-    df_append.to_csv("copilot.csv", index=False)
+    initial = int(df_append.size/15)
+    # print(df_append.head)
+    df_append.drop_duplicates(inplace=True, subset=["TEXT"])
+    final = int(df_append.size/15)
+    df_append.to_csv("input/" + file, index=False)
+    print(f"{dir} initial data size: {initial} Final data size: {final}")
 
-main()
+
+all()
